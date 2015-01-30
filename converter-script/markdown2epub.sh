a9ad26d60265455f0047f80ae361a2fee16c3b14
Florian Eßer <f.esser@rwth-aachen.de>
2015-01-30 15:32:31 +0100

    create subdirectories for converter script and ePub output

---
+++ converter-script/markdown2epub.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+pandoc --smart title.txt ../mark726s-eve-lore-survival-guide-v-2-0.md -o ../ePub/mark726s-eve-lore-survival-guide-v-2-0.epub
--- markdown2epub.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-pandoc --smart title.txt mark726s-eve-lore-survival-guide-v-2-0.md -o mark726s-eve-lore-survival-guide-v-2-0.epub
