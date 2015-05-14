95c620b440416569c2b453757bb6335b49de6537
Florian Eßer <f.esser@rwth-aachen.de>
2015-05-14 11:29:11 +0200

    Compile ePub v2.1

--- converter-script/markdown2epub.sh
+++ converter-script/markdown2epub.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pandoc --smart title.txt ../mark726s-eve-lore-survival-guide-v-2-0.md -o ../ePub/mark726s-eve-lore-survival-guide-v-2-0.epub
+pandoc --smart title.txt ../mark726s-eve-lore-survival-guide-v-2-1.md -o ../ePub/mark726s-eve-lore-survival-guide-v-2-1.epub
